df51e19ab55060a84b9d5f4ce999197e1cdccac5
kikevis <villasanchezg22@gmail.com>
2022-08-19 16:25:23 -0500

    Update

---
+++ general/typeof.js
@@ -0,0 +1,16 @@
+// Uso del typeof
+
+var num = 343;
+
+console.log("num " + typeof(num));
+
+num = "dsdasdsadsad";
+
+console.log("num " + typeof(num));
+
+num = 34445;
+
+console.log("num " + typeof(num));
+
+//Geovanny E. Villa Sánchez
+//KikeViS
