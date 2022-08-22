8b4cb34dfe4d74a14cc4b69937bf21bae9de4ed9
kikevis <villasanchezg22@gmail.com>
2022-08-22 15:22:32 -0500

    Objets

---
+++ objetos/persons.js
@@ -0,0 +1,27 @@
+// Crear un array del objeto Persona con persistencia en adicion
+
+var list = [];
+var exit = false;
+
+while (exit === false) {
+    var name = prompt("nombre?");
+    var age = prompt("edad?");
+    var gender = prompt("genero?");
+
+    list.push(new Person(name, age, gender));
+
+    var cont = prompt("continuar?");
+
+    if (cont === "n")
+        exit = true;
+}
+
+
+function Person(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+}
+
+//Geovanny E. Villa Sánchez
+//KikeViS
